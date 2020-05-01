924595f6e260136d00d40e9813dd189fd88f5d3c
anton <anton.obukhov@vision.ee.ethz.ch>
2020-05-01 22:56:01 +0200

    bisect circleci crash

--- tests/precision/test_metric_isc_fidelity.py
+++ tests/precision/test_metric_isc_fidelity.py
@@ -4,6 +4,7 @@ import sys
 import tempfile
 import unittest
 
+from torch_fidelity.metric_isc import KEY_METRIC_ISC_MEAN, KEY_METRIC_ISC_STD
 from torch_fidelity.utils import json_decode_string
 
 
@@ -36,11 +37,11 @@ class TestMetricIscFidelity(unittest.TestCase):
         )
         self.assertEqual(res.returncode, 0, msg=res)
 
-        # print(f'Running reference ISC...', file=sys.stderr)
-        # res_ref = self.call_ref_isc(cifar10_root, cuda, determinism=True)
-        # self.assertEqual(res_ref.returncode, 0, msg=res_ref)
-        # res_ref = json_decode_string(res_ref.stdout.decode())
-        # print('Reference ISC result:', res_ref, file=sys.stderr)
+        print(f'Running reference ISC...', file=sys.stderr)
+        res_ref = self.call_ref_isc(cifar10_root, cuda, determinism=True)
+        self.assertEqual(res_ref.returncode, 0, msg=res_ref)
+        res_ref = json_decode_string(res_ref.stdout.decode())
+        print('Reference ISC result:', res_ref, file=sys.stderr)
 
         print(f'Running fidelity ISC cached...', file=sys.stderr)
         res_fidelity = self.call_fidelity_isc(cifar10_root)
@@ -48,11 +49,11 @@ class TestMetricIscFidelity(unittest.TestCase):
         res_fidelity = json_decode_string(res_fidelity.stdout.decode())
         print('Fidelity ISC result:', res_fidelity, file=sys.stderr)
 
-        # self.assertAlmostEqual(res_ref[KEY_METRIC_ISC_MEAN], res_fidelity[KEY_METRIC_ISC_MEAN], delta=1e-3)
-        # self.assertAlmostEqual(res_ref[KEY_METRIC_ISC_STD], res_fidelity[KEY_METRIC_ISC_STD], delta=5e-1)
-        # self.assertLess(res_fidelity[KEY_METRIC_ISC_STD], res_ref[KEY_METRIC_ISC_STD])
+        self.assertAlmostEqual(res_ref[KEY_METRIC_ISC_MEAN], res_fidelity[KEY_METRIC_ISC_MEAN], delta=1e-3)
+        self.assertAlmostEqual(res_ref[KEY_METRIC_ISC_STD], res_fidelity[KEY_METRIC_ISC_STD], delta=5e-1)
+        self.assertLess(res_fidelity[KEY_METRIC_ISC_STD], res_ref[KEY_METRIC_ISC_STD])
 
-        # self.assertAlmostEqual(res_fidelity[KEY_METRIC_ISC_MEAN], 11.236778, delta=1e-6)
+        self.assertAlmostEqual(res_fidelity[KEY_METRIC_ISC_MEAN], 11.236778, delta=1e-6)
 
 
 if __name__ == '__main__':
