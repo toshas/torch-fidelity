2762d1f372bad00263d6dd1dcbb7b6a7c63b7eff
anton <anton.obukhov@gmail.com>
2023-05-07 20:26:18 +0200

    fix setup.py

--- setup.py
+++ setup.py
@@ -43,11 +43,11 @@ setup(
     license='Apache License 2.0',
     url='https://www.github.com/toshas/torch-fidelity',
     keywords=[
-        'generative', 'models', 'evaluation', 'metrics' 
+        'generative', 'models', 'evaluation', 'metrics',
         'deep', 'adversarial', 'networks', 'gan', 'diffusion', 'ddpm',
         'inception', 'score', 'frechet', 'distance', 'kernel', 'precision', 'recall', 'f-score',
         'perceptual', 'path', 'length', 'clip', 'isc', 'fid', 'kid', 'lpips', 'prc', 'ppl',
-        'reproducibility', 'fidelity'
+        'reproducibility', 'fidelity',
     ],
     entry_points={
         'console_scripts': [
